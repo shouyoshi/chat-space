f6f3c356a789fc81ebe02a2a994b63ae35540e19
shouyoshi <s.y.5180@au.com>
2020-06-26 14:11:36 +0900

    last commit2

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ $(function() {
      let html = `
            <div class="ChatMember">
            <p class = "ChatMember_name">${users.name}</p> 
-           <div class="ChatMember_add ChatMember_button" data-user-id="2300">追加</div>
+           <div class="ChatMember__add ChatMember__button" data-user-id="${users.id}" data-user-name="${users.name}">追加</div>
            </div>
           `
           return html;
@@ -17,6 +17,17 @@ $(function() {
           `
     return html;
    }
+
+   function addMember(name, id) {
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${name}</p>
+                  <input name="group[user_ids][]" type="hidden" value="${id}" />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                </div>
+                `;
+    $(".ChatMembers").append(html);
+  }
 
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
@@ -43,5 +54,15 @@ $(function() {
     .fail(function() {
       alert("通信エラーです");
     });
+  });
+  $("#UserSearchResult").on("click", ".ChatMember__add", function(){
+    const userName = $(this).attr("data-user-name");
+    console.log(userName);
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    addMember(userName, userId);
+  })
+  $(".ChatMembers").on("click", ".ChatMember__remove", function() {
+    $(this).parent().remove();
   });
 });
